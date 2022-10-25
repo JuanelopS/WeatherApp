8a3ba04ab1a27b036f32bfd2793acaaa690050a1
JuanelopS <contacto@juangavira.me>
2022-10-25 20:13:47 +0200

    Added fetch weather and city data with gps location html api

--- src/components/ActualPosition.jsx
+++ src/components/ActualPosition.jsx
@@ -1,45 +1,82 @@
 import React, { useState, useEffect } from 'react';
+import { ThreeBody } from '@uiball/loaders';
+import { weatherCodes } from '../helpers/weatherCodes';
+import '../styles/ActualPosition.css';
 
 export const ActualPosition = () => {
 
-  const [ lat, setLat ] = useState(null);
-  const [ long, setLong ] = useState(null);
   const [ weather, setWeather ] = useState(null);
-
-  useEffect(() => {
-    getCoords();
-    dataFetch()
-  }, [lat, long]);
-
-  /* Retrieve location through HTML5 api related */
-
-  const geolocationAPI = navigator.geolocation;
+  const [ city, setCity ] = useState(null);
+  const [ isLoading, setIsLoading ] = useState(true);
   
-  const getCoords = () => {
+  /* TODO: AÑADIR TRY/CATCH */
+  /* FETCH CITY */
+
+  const fetchCity = async (lat, long) => {
+    fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=en`)
+    .then(response => response.json())
+    .then(data => setCity(data))
+    .catch(err => console.log(err));
+  }  
+
+  /* FETCH WEATHER */
+
+  const fetchWeather = async (lat, long) => {
+      fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum&current_weather=true&timezone=auto`)
+      .then(response => response.json())
+      .then(data => setWeather(data))
+      .catch(err => console.log(err));
+  }  
+  
+  useEffect(() => {
+
+    const geolocationAPI = navigator.geolocation;
+
     if(!geolocationAPI){
       console.log('Geolocation is not available');
     } else {
-      geolocationAPI.getCurrentPosition( position => {
-        const { coords } = position;
-        setLat(coords.latitude);
-        setLong(coords.longitude);
+      geolocationAPI.getCurrentPosition( async position => {
+        setTimeout(() => setIsLoading(false),3000);
+        // await fetchWeather(position.coords.latitude, position.coords.longitude);
+        // await fetchCity(position.coords.latitude, position.coords.longitude);
       });
-    }
-  }
+    } 
 
-  /* Fetch openweatherAPI data */
+  }, [])
 
-  const dataFetch = async () => {
-    
-    const data = await (fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=48f95418f83113c7fa27c6337592ab1e&units=metric&lang=sp`));
-    const dataJson = await data.json();
-    setWeather(dataJson);
-
-  }
 
   return (
-    <div>
-      { weather.name }
+    <div className='geolocation-weather-card'>
+      <div className='geolocation-weather-text'>
+        <h3> 
+          {
+            isLoading ? 
+            <ThreeBody className='loading'/>
+            :
+            `${city.city} ${city.countryCode} (${weather.latitude} - ${weather.longitude})`
+          }
+        </h3>
+        <h3>
+          {
+            !(isLoading) && `${weatherCodes[weather.current_weather.weathercode]}`
+          }
+        </h3>
+        <h3>
+          {
+          !(isLoading) && `Current temperature: ${weather.current_weather.temperature}º`
+          }
+        </h3>
+        <h3>
+          {
+          !(isLoading) && `Wind Speed: ${weather.current_weather.windspeed}km/h`
+          }
+        </h3>
+        <h3>
+          {
+          !(isLoading) && `Rain: ${weather.daily.precipitation_sum[0]} mm`
+          }
+        </h3>
+      </div>
     </div>
   )
 }
